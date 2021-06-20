a9fe272839546027b651a1ffe82a8d833a288164
fzhOS <1569818140@qq.com>
2021-06-20 20:52:42 +0800

    modified some syntax

--- day01/非固定参数.py
+++ day01/非固定参数.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python 
 # coding:utf-8
-def stu_register(name,age,*args)  #*args会把多传入的参数变为tuple ,
+def stu_register(name,age,*args):  #*args会把多传入的参数变为tuple ,
     print(name,age,args[0],*args,args)
 stu_register('fzhOS',21,'money','mac','switch','games','music')
 stu_register('fzh',21,'delicious food')
